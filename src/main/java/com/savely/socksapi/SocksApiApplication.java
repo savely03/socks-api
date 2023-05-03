02bff5842de8712397719846dc404898723d8549
Savely Domnikov <savely.domnikov@yandex.ru>
2023-05-03 20:46:59 +0300

    Initial commit

---
+++ src/main/java/com/savely/socksapi/SocksApiApplication.java
@@ -0,0 +1,13 @@
+package com.savely.socksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocksApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/savely/socksapi/SocksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savely.socksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
